24726151339f50b4e6ebe00967a1eb444dfeaf9c
Angelo Ceciliano Ortega <85136323+Angelo-CE@users.noreply.github.com>
2026-05-12 16:33:12 -0600

    Clase de Producto
    Se realizo la clase producto, sus atributos y función.

---
+++ object_interface.py
@@ -0,0 +1,10 @@
+from Objects.Producto import Producto
+
+# Prueba
+# lapiz = Producto(1, "Lapiz", 10, 20)
+
+# print(lapiz.id)  
+# print(lapiz.nombre)  
+# print(lapiz.tiempo_ent)  
+# print(lapiz.tiempo_sal)  
+# print(lapiz.get_tiempo_total())  
